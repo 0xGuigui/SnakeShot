51495c0af4db169985be4df1857f21d27c275894
0xGuigui <guilhem.jeh@gmail.com>
2024-03-19 13:48:46 +0100

    Ajouter la fonctionnalité de chargement des commandes dans le prompt

--- src/shell/prompt.py
+++ src/shell/prompt.py
@@ -8,17 +8,28 @@
 
 from include.lib import *
 from include.funcs_library import *
+import os
+import importlib.util
+from glob import glob
 
-def prompt():
+def load_commands():
+    commands = {}
+    command_files = glob("src/shell/commands/*.py")
+    for file_path in command_files:
+        file_name = os.path.basename(file_path)[:-3]  # Remove the .py extension
+        spec = importlib.util.spec_from_file_location(file_name, file_path)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        commands[file_name] = module
+    return commands
+
+def prompt(si):
+    commands = load_commands()
     while True:
         command = input("SnakeShot $ ")
         if command == "exit":
             break
-        elif command in os.listdir("src/shell/commands"):
-            try:
-                module = __import__(f"src.shell.commands.{command}", fromlist=[""])
-                getattr(module, command)()
-            except (ImportError, AttributeError):
-                print(f"Error executing {command}")
+        elif command in commands:
+            commands[command].run(si)
         else:
             print("Command not found")
