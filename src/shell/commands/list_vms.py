5b7a17f296b6ce3e59369300fb1e0d7342a8f083
0xGuigui <guilhem.jeh@gmail.com>
2024-04-02 12:02:58 +0200

    Refactor list_vms.py to sort and print VM names

--- src/shell/commands/list_vms.py
+++ src/shell/commands/list_vms.py
@@ -15,10 +15,18 @@ def list_vms(si):
     """
     List all VMs.
     """
+
+    # Get content
     content = si.RetrieveContent()
+
+    # Get VMs
     container = content.viewManager.CreateContainerView(content.rootFolder, [vim.VirtualMachine], True)
     vm_names = [vm.name for vm in container.view]
+
+    # Sort VM names
     vm_names.sort()
+
+    # Print VM names
     for vm_name in vm_names:
         print(vm_name)
     container.Destroy()
