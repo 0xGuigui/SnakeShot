b6d6cebe85512c6baa8eca39a9149db57c32ac93
0xGuigui <0xguigui@proton.me>
2024-04-09 13:13:39 +0200

    Ajout de la fonction power_on pour allumer une machine virtuelle

---
+++ src/shell/commands/power_on.py
@@ -0,0 +1,30 @@
+##
+##  power_on.py
+##  src/shell/commands
+##
+##  Created by 0xGuigui on 02/04/2024.
+##  Contributor(s): 0xGuigui
+##
+
+from include.lib import *
+import get_obj
+
+def run_power_on(si):
+    vm_name = input("Enter the name of the VM you want to power on: ")
+    power_on(si, vm_name)
+
+def power_on(si, vm_name):
+    content = si.RetrieveContent()
+    vm = get_obj(content, [vim.VirtualMachine], vm_name)
+
+    if vm:
+        try:
+            if vm.runtime.powerState == 'poweredOn':
+                print(f"The virtual machine {vm_name} is already powered on.")
+                return
+            task = vm.PowerOnVM_Task()
+            print(f"Powering on the virtual machine {vm_name}...")
+        except Exception as e:
+            print(f"An error occurred while powering on the virtual machine {vm_name}: {e}")
+    else:
+        print(f"Could not find a VM with the name {vm_name}.")
