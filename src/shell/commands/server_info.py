1dced78db93b4ce062b8282157692057539909c9
0xGuigui <0xguigui@proton.me>
2024-04-09 14:43:04 +0200

    Refactor server_info.py to update server information printing and use about variable

--- src/shell/commands/server_info.py
+++ src/shell/commands/server_info.py
@@ -1,33 +1,30 @@
-##
-##  server_info.py
-##  src/shell/commands
-##
-##  Created by 0xGuigui on 02/04/2024.
-##  Contributor(s): 0xGuigui
-##
-
-from include.lib import *
-from src.get_obj import *
-
-def run_server_info(si):
-    server_info(si)
-
 def server_info(si):
-    """
-    Print information about the vSphere server.
-    """
+    # Récupération des informations de l'hôte connecté
     content = si.RetrieveContent()
-    about_info = content.about
-    host_system = get_obj(content, [vim.HostSystem])
+    about = content.about
 
-    print("Server information:")
-    print(f"IP Address: {host_system.summary.managementServerIp}")
-    print(f"Name: {about_info.name}")
-    print(f"Full Name: {about_info.fullName}")
-    print(f"Vendor: {about_info.vendor}")
-    print(f"Version: {about_info.version}")
-    print(f"Build: {about_info.build}")
-    print(f"OS Type: {about_info.osType}")
-    print(f"Product Line ID: {about_info.productLineId}")
-    print(f"API Type: {about_info.apiType}")
-    print(f"API Version: {about_info.apiVersion}")
+    print("Server Information:")
+    print("-------------------")
+    print("Product Name:", about.fullName)
+    print("Product Version:", about.version)
+    print("Vendor:", about.vendor)
+    print("Build:", about.build)
+    print("Instance UUID:", about.instanceUuid)
+    print("License Product Name:", about.licenseProductName)
+    print("License Product Version:", about.licenseProductVersion)
+
+    # Récupération des informations sur les hôtes
+    host_view = content.viewManager.CreateContainerView(content.rootFolder, [vim.HostSystem], True)
+    hosts = host_view.view
+    host_view.Destroy()
+
+    print("\nHost Information:")
+    print("-----------------")
+    for host in hosts:
+        print("Host:", host.name)
+        print("IP Address:", host.summary.managementServerIp)
+        print("Bios Version:", host.hardware.systemInfo.biosInfo.biosVersion)
+        print("CPU Model:", host.hardware.cpuModel)
+        print("CPU Cores:", host.hardware.cpuInfo.numCpuCores)
+        print("Memory:", round(host.hardware.memorySize / (1024 * 1024 * 1024), 2), "GB")
+        print("")
